a4840133f815d3fd6e207965760ac1600dbefdef
cheroliv <cheroliv@laposte.net>
2020-09-03 20:15:51 +0200

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Link} from "gatsby";
+
+export default function About() {
+    return <>
+        <h1>About Us</h1>
+        <Link to="/">&larr; back to home</Link>
+    </>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import {Link} from "gatsby";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <>
+  <h1>Welcome To Our Studio!</h1>
+    <p>It's Nice To Meet You</p>
+    <Link to="/about/">Learn about me &rarr;</Link>
+  </>
 }

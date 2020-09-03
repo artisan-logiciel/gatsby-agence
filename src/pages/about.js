89cff52b52dd73e5ecfec81fca9fbabff2ed6bdd
cheroliv <cheroliv@laposte.net>
2020-09-04 00:47:06 +0200

    added some layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {Link} from "gatsby";
+import Layout from "../components/layout";
 
 export default function About() {
-    return <>
+    return <Layout>
         <h1>About Us</h1>
         <Link to="/">&larr; back to home</Link>
-    </>
+    </Layout>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import {Link} from "gatsby";
+import Layout from "../components/layout";
 
 export default function Home() {
-  return <>
-  <h1>Welcome To Our Studio!</h1>
-    <p>It's Nice To Meet You</p>
-    <Link to="/about/">Learn about me &rarr;</Link>
-  </>
-}
+    return <Layout>
+        <h1>Welcome To Our Studio!</h1>
+        <p>It's Nice To Meet You</p>
+        <Link to="/about/">Learn about me &rarr;</Link>
+    </Layout>
+};

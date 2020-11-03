bd63766191863c41e811a9e7ff77423e06eaeeed
cheroliv <cheroliv@laposte.net>
2020-11-04 00:44:55 +0100

    add gatsby-source-filesystem plugin and settings in gatsby-config.js, the commande line to add is: npm i gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,20 @@ module.exports = {
     },
     plugins: [
         "gatsby-plugin-emotion",
-        "gatsby-plugin-react-helmet", {
+        "gatsby-plugin-react-helmet",
+        {
             resolve: 'gatsby-plugin-mdx',
             options: {
                 defaultLayouts: {
                     default: require.resolve('./src/components/layout.js'),
                 },
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'posts',
+                path: 'posts',
             },
         },
     ],

2a67e70780f8a8a172d90d2d7ae350ae5833b535
cheroliv <cheroliv@laposte.net>
2020-11-04 16:51:50 +0100

    add gatsby-node.js to handle ticket page creation and url by query on gatsby grapql engine.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout";
 
 export default function About() {
     return <Layout>
+        {/*date: */}02-11-2020<br/>
         <Link to="/">&larr; back to home</Link>
     </Layout>
 }

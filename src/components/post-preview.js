b772ce457d9585169c92674a2339fcc456c86951
cheroliv <cheroliv@laposte.net>
2020-11-06 21:56:12 +0100

    add image preview on posts and realisation list.

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -5,34 +5,22 @@ import Image from 'gatsby-image';
 import ReadLink from '../components/read-link';
 
 const PostPreview = ({post}) => (
-    <article
-        css={css`
+    <article css={css`
       border-bottom: 1px solid #ddd;
       display: flex;
       margin-top: 0;
       padding-bottom: 1rem;
-
-      :first-of-type {
-        margin-top: 1rem;
-      }
-    `}>
-        <Link
-            to={post.slug}
-            css={css`
-        margin: 1rem 1rem 0 0;
-        width: 100px;`}>
-            <Image
-                css={css`* {margin-top: 0;}`}
-                fluid={post.image.sharp.fluid}
-                alt={post.title}/>
+      :first-of-type { margin-top: 1rem;}`}>
+        <Link to={post.slug}
+              css={css`margin: 1rem 1rem 0 0; width: 100px;`}>
+            <Image alt={post.title}
+                   css={css`* {margin-top: 0;}`}
+                   fluid={post.image.sharp.fluid}/>
         </Link>
-
         <div>
-            <h3>
-                <Link to={post.slug}>{post.title}</Link>
-            </h3>
+            <h3><Link to={post.slug}>{post.title}</Link></h3>
             <p>{post.excerpt}</p>
-            <ReadLink to={post.slug}>read this post &rarr;</ReadLink>
+            <ReadLink to={post.slug}>lire cet article &rarr;</ReadLink>
         </div>
     </article>
 );
--- src/components/realisation-preview.js
+++ src/components/realisation-preview.js
@@ -5,35 +5,22 @@ import Image from 'gatsby-image';
 import ReadLink from '../components/read-link';
 
 const RealisationPreview = ({realisation}) => (
-    <article
-        css={css`
+    <article css={css`
       border-bottom: 1px solid #ddd;
       display: flex;
       margin-top: 0;
       padding-bottom: 1rem;
-
-      :first-of-type {
-        margin-top: 1rem;
-      }
-    `}
-    >
-        <Link
-            to={realisation.slug}
-            css={css`
-        margin: 1rem 1rem 0 0;
-        width: 100px;`}>
-            <Image
-                css={css`* {margin-top: 0;}`}
-                fluid={realisation.image.sharp.fluid}
-                alt={realisation.title}/>
+      :first-of-type { margin-top: 1rem;}`}>
+        <Link to={realisation.slug}
+              css={css`margin: 1rem 1rem 0 0;width: 100px;`}>
+            <Image alt={realisation.title}
+                   css={css`* {margin-top: 0;}`}
+                   fluid={realisation.image.sharp.fluid}/>
         </Link>
-
         <div>
-            <h3>
-                <Link to={realisation.slug}>{realisation.title}</Link>
-            </h3>
+            <h3><Link to={realisation.slug}>{realisation.title}</Link></h3>
             <p>{realisation.excerpt}</p>
-            <ReadLink to={realisation.slug}>see this realisation &rarr;</ReadLink>
+            <ReadLink to={realisation.slug}>voir cette réalisation &rarr;</ReadLink>
         </div>
     </article>
 );

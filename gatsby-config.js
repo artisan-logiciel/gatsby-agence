d879f25716d0f760fb48822beeddd514427c6e55
cheroliv <cheroliv@laposte.net>
2020-11-06 16:04:10 +0100

    add images folder to source path

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,21 @@ module.exports = {
     },
     plugins: [
         "gatsby-plugin-emotion",
-        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-react-helmet",'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
         {
             resolve: 'gatsby-plugin-mdx',
             options: {
                 defaultLayouts: {
                     default: require.resolve('./src/components/layout.js'),
                 },
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: 'images',
             },
         },
         {

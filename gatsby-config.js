fa2f1b3adb8cbb994b7085611ab1b2321024997e
cheroliv <cheroliv@laposte.net>
2020-11-09 01:27:44 +0100

    register and configure gatsby-plugin-webpack-bundle-analyzer plugin in gatsby-config.js.

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,5 +45,14 @@ module.exports = {
                 username: 'build_funnels',
             },
         },
+        {
+            resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+            options: {
+                production: true,
+                disable: !process.env.ANALYZE_BUNDLE_SIZE,
+                generateStatsFile: true,
+                analyzerMode: 'static',
+            },
+        },
     ],
 };

b64c10525b896072148baaafec62282827f4b47c
cheroliv <cheroliv@laposte.net>
2020-11-12 16:58:58 +0100

    update index.js to add contant to home page and add email to contact.mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 
@@ -6,7 +7,36 @@ const Home = () =>
   <>
     <Hero />
     <Layout>
+
       <h1>Améliorer votre expérience digital.</h1>
+
+
+      <h2>Comment j'interviens?</h2>
+      <ol>
+        <li>Analyse des besoins, identifications des workflows pour répondre à la demande.</li>
+        <li>Prototypage et budget.</li>
+        <li>Développement d'une solution.</li>
+        <li>Déploiment et exploitation de la solution.</li>
+      </ol>
+
+      <h2>Mes outils</h2>
+
+
+      <h3>Méthodologies:</h3> Agile, scrum, kanban et merise.
+      <h3>Mobile:</h3> AndroidStudio et Kotlin.
+
+
+      <h3>Front side:</h3> Gatsby et Reactjs.
+
+      <h3>Server side:</h3>
+      <ul css={css`list-style-type:none;`}>
+        <li>Spring-Boot et Kotlin</li>
+        <li>PostgreSQL</li>
+        <li>Node.js et Javascript</li>
+        <li>Docker et Kubernetes</li>
+      </ul>
+      <h3>Design:</h3> Figma
+
     </Layout>
   </>
 export default Home

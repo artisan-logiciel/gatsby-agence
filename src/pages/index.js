de252765c137f4e3c8d4041aa74487a6c9a16d3c
cheroliv <cheroliv@laposte.net>
2020-11-12 18:43:43 +0100

    update index.js to add contant to home page and add email to contact.mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ const Home = () =>
     <Hero />
     <Layout>
       <h1>Améliorer votre expérience digital.</h1>
+      {/*Plus de détails*/}
+
       <h2>Comment j'interviens?</h2>
       <ol>
         <li>Analyse des besoins, identifications des traitements.</li>
@@ -15,6 +17,7 @@ const Home = () =>
         <li>Développement d'une solution.</li>
         <li>Déploiement et exploitation de la solution.</li>
       </ol>
+      {/*Pour cela, voici mes outils*/}
       <h2>Mes outils</h2>
       <h3>Méthodologies:</h3> Agile, scrum, kanban et merise.
       <h3>Design:</h3> Figma.
@@ -23,8 +26,8 @@ const Home = () =>
       <h3>Server side:</h3>
       <ul css={css`list-style-type:none;`}>
         <li>Spring-Boot et Kotlin</li>
-        <li>PostgreSQL</li>
-        <li>Javascript et Node.js</li>
+        <li>PostgreSQL, MySQL, H2 et Firebase</li>
+        <li>Javascript, Node.js et ExpressJs</li>
         <li>Docker et Kubernetes</li>
       </ul>
     </Layout>

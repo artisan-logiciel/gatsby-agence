e0859dc59407a70d8b8bd5091d5127cd86ceefa4
cheroliv <cheroliv@laposte.net>
2020-11-12 19:14:01 +0100

    explode home component content into variables.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,16 @@ import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 
-const Home = () =>
-  <>
-    <Hero />
-    <Layout>
+const Home = () => {
+
+  const mojo =
+    <>
       <h1>Améliorer votre expérience digital.</h1>
       {/*Plus de détails*/}
+    </>
 
+  const howIdo =
+    <>
       <h2>Comment j'interviens?</h2>
       <ol>
         <li>Analyse des besoins, identifications des traitements.</li>
@@ -18,6 +21,10 @@ const Home = () =>
         <li>Déploiement et exploitation de la solution.</li>
       </ol>
       {/*Pour cela, voici mes outils*/}
+    </>
+
+  const tools =
+    <>
       <h2>Mes outils</h2>
       <h3>Méthodologies:</h3> Agile, scrum, kanban et merise.
       <h3>Design:</h3> Figma.
@@ -30,6 +37,15 @@ const Home = () =>
         <li>Javascript, Node.js et ExpressJs</li>
         <li>Docker et Kubernetes</li>
       </ul>
+    </>
+
+  return <>
+    <Hero />
+    <Layout>
+      {mojo}
+      {howIdo}
+      {tools}
     </Layout>
   </>
+}
 export default Home

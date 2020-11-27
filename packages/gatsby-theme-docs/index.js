327bb15d3e33604cac4f131a17fc8e56bc4c2a99
cheroliv <cheroliv@laposte.net>
2020-11-27 04:44:57 +0100

    commit everything to bring code from clone

--- packages/gatsby-theme-docs/index.js
+++ packages/gatsby-theme-docs/index.js
@@ -1,1 +1,1 @@
-gatsby-theme-docs
+//gatsby-theme-docs
--- packages/gatsby-theme-docs/src/components/table-of-contents.js
+++ packages/gatsby-theme-docs/src/components/table-of-contents.js
@@ -2,8 +2,11 @@
 import { jsx } from "theme-ui"
 import { graphql, Link, useStaticQuery } from "gatsby"
 
-const TableOfContents = () => {
-  const data = useStaticQuery(graphql`
+const TableOfContents = () =>
+  <div>
+    <h2>Explore the Docs</h2>
+    <ul>
+      {useStaticQuery(graphql`
     query {
       allDocsPage {
         nodes {
@@ -13,34 +16,22 @@ const TableOfContents = () => {
         }
       }
     }
-  `)
-
-  const pages = data.allDocsPage.nodes
-
-  return (
-    <div>
-      <h2>Explore the Docs</h2>
-      <ul>
-        {pages.map(({ id, path, title }) => (
+  `).allDocsPage.nodes.map(
+        ({ id, path, title }) =>
           <li key={id}>
-            <Link
-              to={path}
-              sx={{
-                "&.active": {
-                  fontStyle: "italic",
-                  textDecoration: "none",
-                  "::after": { content: "\" (currently viewing)\"" }
-                }
-              }}
-              activeClassName="active"
-            >
+            <Link to={path}
+                  activeClassName="active"
+                  sx={{
+                    "&.active": {
+                      fontStyle: "italic",
+                      textDecoration: "none",
+                      "::after": { content: "\" (currently viewing)\"" }
+                    }
+                  }}>
               {title}
             </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+          </li>)}
+    </ul>
+  </div>
 
 export default TableOfContents

6c2a66ddf4a9f5287c8eccdbc85ce8d23016f73a
cheroliv <cheroliv@laposte.net>
2020-12-08 19:30:50 +0100

    refactor: object destructuring

--- packages/gatsby-theme-docs/src/components/docs-page.js
+++ packages/gatsby-theme-docs/src/components/docs-page.js
@@ -1,26 +1,21 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-import Layout from './layout';
-import TableOfContents from './table-of-contents';
+import { jsx } from "theme-ui"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import Layout from "./layout"
+import TableOfContents from "./table-of-contents"
 
-const DocsPage = ({ page }) => (
+const DocsPage = ({ page: { body, title, updated } }) =>
   <Layout>
-    <h1>{page.title}</h1>
-    <MDXRenderer>{page.body}</MDXRenderer>
-    <p
-      sx={{
-        borderTop: theme => `1px solid ${theme.colors.muted}`,
-        color: 'muted',
-        fontSize: 14,
-        mt: 2,
-        pt: 2,
-      }}
-    >
-      This page was updated {page.updated}.
-    </p>
+    <h1>{title}</h1>
+    <MDXRenderer>{body}</MDXRenderer>
+    <p sx={{
+      borderTop: ({ colors: { muted } }) => `1px solid ${muted}`,
+      color: "muted",
+      fontSize: 14,
+      mt: 2,
+      pt: 2
+    }}> This page was updated {updated}. </p>
     <TableOfContents />
   </Layout>
-);
 
-export default DocsPage;
+export default DocsPage

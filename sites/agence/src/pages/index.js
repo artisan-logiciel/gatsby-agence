c6256550f164196bc93f8d3fb2552f67529a60f0
cheroliv <cheroliv@laposte.net>
2020-12-09 22:08:01 +0100

    add domain-facade.js component

--- sites/agence/src/pages/index.js
+++ sites/agence/src/pages/index.js
@@ -1,14 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
-// import DomainFacade from "../components/domain/domain-facade"
+import DomainFacade from "../components/domain/domain-facade"
 
 const Home = () =>
-
   <>
     <Hero />
     <Layout>
-      {/*<DomainFacade />*/}
+      <DomainFacade />
     </Layout>
   </>
 
